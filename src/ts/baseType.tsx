89ed41fa1e25aa168caa0a8ce5324a1d3b2f981d
caoxuy2333 <120170535@qq.com>
2022-11-09 19:44:47 +0800

    基础类型

--- src/ts/baseType.tsx
+++ src/ts/baseType.tsx
@@ -16,13 +16,43 @@ const arr: Array<number> = [1, 2, 3, 4];
 const yz: [string, number] = ['1', 2];
 
 // 枚举
-enum Color { red='a', green='b', blue='c' }
+enum Color { red = 'a', green = 'b', blue = 'c' }
 const c: Color = Color.green;
+
+enum Addr { gz, sh, k = 9, p = 9, o = 9 }
+let l = Addr[9];
+console.log(l);
+
+// void -- 返回void 表示没有任何类型 undefined / null
+export const vo = function (): void {
+    return;
+};
+
+// undefined  null
+const un: undefined = null;
+const nu: null = undefined;
+
+// Never  返回无法到达的结果
+export const ne =function(): never{
+    throw new Error('');
+};
+
+// Object
+export const obj = function(): object{
+    return {};
+};
 
 export const View = function () {
     console.log(arr);
     console.log(yz);
     console.log(c);
     return <div>{isDone ? num : str}</div>;
+};
+
+
+export const Temp = function () {
+    return <div>
+        {Color.red}
+    </div>;
 };
 
