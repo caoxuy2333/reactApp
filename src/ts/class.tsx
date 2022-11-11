ab7836381492b4177192a432d1a0b135f1918753
caoxuy2333 <120170535@qq.com>
2022-11-11 11:19:28 +0800

    class

--- src/ts/class.tsx
+++ src/ts/class.tsx
@@ -4,7 +4,7 @@ class A {
         this.name = name;
     }
     move(a: number, b: number) {
-        console.log(a + b);
+        console.log(a + b, this.name);
     }
 }
 
@@ -21,13 +21,32 @@ let faster = new A('1');
 let child = new B('2');
 console.log(faster);
 console.log(child);
-child.move(1,2);
+child.move(1, 2);
 
 
+// 公有, 私有
 
+class Q {
+    public name: string = '1'; // 共有的
+    private code: string = '2'; // 私有的
+    protected id: number = 3; // 私有的, 但能在派生类访问
+    // 获取私有值
+    getCo() {
+        return this.code;
+    }
+}
+class W extends Q {
+    max(a: number, b: number) {
+        console.log(this);
+        console.log(this.name);
+        // console.log(this.code); 错误的
+        console.log(this.getCo()); // 获取私有值
+        console.log(this.id);
+    }
+}
 
-
-
+let w = new W();
+w.max(1, 2);
 
 
 
