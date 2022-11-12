ed5c1bceb8fef8e00322421b91a7075108c321f7
caoxuy2333 <120170535@qq.com>
2022-11-12 14:09:32 +0800

    class

--- src/ts/class.tsx
+++ src/ts/class.tsx
@@ -28,25 +28,51 @@ child.move(1, 2);
 
 class Q {
     public name: string = '1'; // 共有的
-    private code: string = '2'; // 私有的
+    private _code: string = '2'; // 私有的
     protected id: number = 3; // 私有的, 但能在派生类访问
+    readonly per: string; // 只读属性, 只能在声明时或构造函数里初始化
+    constructor(code: string) {
+        this.code = code;
+        this.per = '4';
+    }
     // 获取私有值
-    getCo() {
-        return this.code;
+    get code() {
+        // this.per  ='5'; // 错误的, 只读属性不能赋值\
+        return this._code;
+    }
+    //
+    set code(val:string){
+        this._code = val;
     }
 }
 class W extends Q {
-    max(a: number, b: number) {
+    constructor(c: string) {
+        super('30');
+    }
+    max(a: number, b: number): boolean {
         console.log(this);
         console.log(this.name);
-        // console.log(this.code); 错误的
-        console.log(this.getCo()); // 获取私有值
+        // console.log(this._code); 错误的 私有值不可获取
+        console.log(this.code); // 获取私有值
         console.log(this.id);
+        return a > b;
     }
 }
 
-let w = new W();
+let w = new W('20');
 w.max(1, 2);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
