67912bf326b8e5e6ff4af85adc5f46d688895c74
caoxuy2333 <120170535@qq.com>
2022-12-16 18:08:36 +0800

    mock

--- src/fetch/mock/index.ts
+++ src/fetch/mock/index.ts
@@ -1,11 +1,8 @@
- 
 const Mock = require('mockjs')
- 
-
 
 Mock.mock(/login/, () => {
   return {
     status: 200,
     message: "获取数据成功2"
   }
-}) 
+})
--- src/ts/13-jsx.tsx
+++ src/ts/13-jsx.tsx
@@ -2,35 +2,35 @@ import * as React from 'react';
 // 使用装饰器 增加Form对象
 
 let Form = function (fun: Function) {
-    console.log('funccccccccccc', fun, fun.prototype);
-    fun.prototype.form = new FormData();
-    return function(){
+  console.log('funccccccccccc', fun, fun.prototype);
+  fun.prototype.form = new FormData();
+  return function () {
 
-    }
-    // fun();
-    // return function (a: object, b: string, d: PropertyDescriptor) {
+  }
+  // fun();
+  // return function (a: object, b: string, d: PropertyDescriptor) {
 
-    // }
+  // }
 }
 
 // @Form()
 class Index extends React.Component {
-    constructor(props: any) {
-        super(props);
-        console.log('ffffffffffffffffffffformdata=>', props);
-        console.log(this);
-        console.log(props);
-    }
+  constructor(props: any) {
+    super(props);
+    console.log('ffffffffffffffffffffformdata=>', props);
+    console.log(this);
+    console.log(props);
+  }
 
-    render() {
-        return (
-            <div>1</div>
-        )
-    }
-}
+  render() {
+    return (
+      <div>1</div>
+    )
+  }
+} 
 
 let View = function () {
-    return <Index />
+  return <Index />
 }
 
 export default Index;
