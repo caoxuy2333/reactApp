2ab40f3cf2a59713f21b407043dbeb0bc4803c6d
caoxuy2333 <120170535@qq.com>
2023-10-11 09:18:40 +0800

    home

--- src/link-router/index.tsx
+++ src/link-router/index.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
+import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import beianIcon from 'assets/备案图标.png'
 import sty from './index.less';
 
 const Index: React.FC = (): JSX.Element => {
-
-
   return (
-    <div>
+    <Fragment>
       {/* <h3>我是首页</h3> */}
       {/* <div>
         <Link to={'/fileTransferProtocol'}>断点续传</Link>
@@ -32,25 +31,24 @@ const Index: React.FC = (): JSX.Element => {
       </div>
       <div className={sty.flex}>
         <Link className={sty.fc} to={'/fc'}>
-          <div>fc-game</div>
+          fc-game
         </Link>
         <Link className={sty.gba} to={'/gba'}>
-          <div>gba-game</div>
+          gba-game
         </Link>
       </div>
       <div className={sty.foot}>
-        <a style={{paddingRight: 20}} href='https://beian.miit.gov.cn/#/Integrated/index'>粤ICP备2023078446号</a>
-         
+        <a style={{ paddingRight: 20 }} href='https://beian.miit.gov.cn/#/Integrated/index'>粤ICP备2023078446号</a>
         <a
-            target="_blank"
-            rel="noreferrer"
-            href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44010602011882"
-          >
-            <img src={beianIcon} style={{ float: 'left' }} />
-             粤公网安备 44010602011882号 
-          </a>
+          target="_blank"
+          rel="noreferrer"
+          href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44010602011882"
+        >
+          <img src={beianIcon} style={{ float: 'left' }} />
+          粤公网安备 44010602011882号
+        </a>
       </div>
-    </div>
+    </Fragment>
   )
 }
 
